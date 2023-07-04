ac5a4b9b50dba66291e0f65b3093a642a2002aad
Jokerowner1st <138540273+Jokerowner1st@users.noreply.github.com>
2023-07-04 06:12:13 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,10 +2,10 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Astra</h1>
-            <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
+            <h1 className="font-semibold text-3xl text-white/75">Brux için Gizlilik Politikası</h1>
+            <p className="text-sm text-white/30 mb-4">Last Update: 07/04/2023  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the Astra bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ Brux botu kullanmak için discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekmektedir. <a className="text-white">(MODERASYON BOTU BRUX TEAM)</a></p>
         <div className="py-3"></div>
         <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot when using certain commands from the bot commands.</p>
         <div className="py-3"></div>
